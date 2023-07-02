99db4e62fb68b8ee8d55bd7137d940496d24068d
TheVietboi <51455274+TheVietboi@users.noreply.github.com>
2023-07-02 23:33:19 +0630

    update

---
+++ buton.js
@@ -0,0 +1,25 @@
+function displayside()
+{
+    var a = document.getElementById('sides').style.width;
+    if (a != '280px') 
+        document.getElementById('sides').style.width = '280px';
+    else document.getElementById('sides').style.width = '90px';
+}
+
+function changeTongquan()
+{
+    document.getElementById('test').innerHTML = 'Sơ lược tổng quan';
+    document.getElementById('show').innerHTML = document.getElementById('board1').innerHTML;
+}
+
+function changeTimkiem()
+{
+    
+    document.getElementById('test').innerHTML = 'Tìm kiếm';
+    document.getElementById('show').innerHTML = document.getElementById('board2').innerHTML;
+}
+
+function defaultScreen()
+{
+    changeTimkiem();
+}
